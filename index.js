a296a9d8d4db959253f4e297923f629011fafc51
Andre Dragacevac <a.dragacevac@icloud.com>
2022-08-25 16:26:13 -0400

    commit tree 9c72733e08abc4b15f470d91a1851c728f0e88ed

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+return string.toUpperCase();
+}
+"Hello!".toUpperCase();
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+"HeLLO".toLowerCase();
+
+sayHello();
+function logShout(hello){
+    console.log("hello".toUpperCase());
+}
+
+sayHello();
+function logWhisper(hello){
+    console.log("Hello".toLowerCase());
+}
+
+sayHello();
+function sayHiToHeadphonedRoommate(string) 
+{
+    if(string.toLowerCase("hello")){
+        return `I can\'t hear you!`;
+    }
+    if(string.toUpperCase("HELLO")){
+        return "YES INDEED!";
+    }
+}
